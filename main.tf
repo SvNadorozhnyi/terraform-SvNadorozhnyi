b0a49b5109a5698bf1fe35eb4001016a533177f2
Sviatoslav Nadorozhnyi <104847887+SvNadorozhnyi@users.noreply.github.com>
2023-08-02 13:13:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
-# main.tf
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx-container"
+  image = "nginx:latest"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+resource "docker_container" "mariadb" {
+  name  = "mariadb-container"
+  image = "mariadb:latest"
+  env   = [
+    "MYSQL_ROOT_PASSWORD=${var.db_root_password}"
+  ]
+  ports {
+    internal = 3306
+    external = 3306
+  }
+}
